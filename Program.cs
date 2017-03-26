7d59732ec378c7e9f8eab47aede7c2f3c73d95d3
cloudman88 <adamanan88@gmail.com>
2017-03-26 12:32:04 +0300

    small manual revert

--- Program.cs
+++ Program.cs
@@ -7,12 +7,8 @@ namespace Genetics
         static void Main(string[] args)
         {
             Manager man = new Manager();
-            DateTime currentDate = DateTime.Now;
             do
-            {
-                Console.WriteLine("   {0:N0} ticks", currentDate.Ticks);                
-                Console.WriteLine(currentDate.Ticks); 
-                    
+            {                    
                 man.Run();
             } while (Console.ReadKey(true).Key != ConsoleKey.Escape);
         }
